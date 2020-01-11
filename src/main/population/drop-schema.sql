4064956127b93ddbc15b1109eb0f1e76ef936143
Vanessa <vanprafer@alum.us.es>
2020-01-11 22:45:54 +0100

    Removed provider of the system

--- src/main/population/drop-schema.sql
+++ src/main/population/drop-schema.sql
@@ -79,10 +79,6 @@
        drop 
        foreign key `FK162v6eiogk4jr8ykjoe80255x`;
 
-    alter table `provider` 
-       drop 
-       foreign key FK_b1gwnjqm6ggy9yuiqm0o4rlmd;
-
     alter table `sponsor` 
        drop 
        foreign key FK_20xk0ev32hlg96kqynl6laie2;
@@ -144,8 +140,6 @@
     drop table if exists `participant`;
 
     drop table if exists `pradas_bulletin`;
-
-    drop table if exists `provider`;
 
     drop table if exists `request`;
 
